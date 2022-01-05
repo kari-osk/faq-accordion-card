5ab5e958acd2577d231001ac4f9f7d246235aa1f
kari-osk <karinaosuka@gmail.com>
2022-01-05 14:56:03 -0300

    Fixed position

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// var accordion = document.querySelectorAll('.accordion')
-// var i
-
-// for (i = 0; i < accordion.length; i++) {
-//   accordion[i].addEventListener('click', function () {
-//     this.classList.toggle('active')
-//     var panel = this.nextElementSibling
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + 'px'
-//     }
-//   })
-// }
-
 var accordion = document.querySelectorAll('.accordion')
 var i
 
